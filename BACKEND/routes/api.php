39edb12eacd4b66760ae3d0174eeb36bf1dcf5b2
ikromulabid <ikromulabid00@gmail.com>
2023-05-29 22:29:48 +0700

    Abid: Upload CRUD Produk

---
+++ BACKEND/routes/api.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Produk;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route untuk tampil data
+Route::get('/tampil', [Produk::class, 'tampil']);
+// Route Untuk detail
+Route::get('/detail/{parameter}', [Produk::class, 'detail']);
+// Route Untuk hapus data
+Route::delete('/delete/{parameter}', [Produk::class, 'delete']);
+// Route Untuk tambah data
+Route::post('/insert', [Produk::class, 'insert']);
+// Route untuk update data kamar
+Route::put('/updateProduk/{parameter}', [Produk::class, 'updateProduk']);
+
