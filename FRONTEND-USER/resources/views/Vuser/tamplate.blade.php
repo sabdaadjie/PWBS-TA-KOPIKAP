5b5b71f82742529f3316cc13b737094868f3fa11
sabdaadjie <55336512+sabdaadjie@users.noreply.github.com>
2023-05-31 22:35:09 +0700

    adjie-Buat tampilan contact

--- FRONTEND-USER/resources/views/Vuser/tamplate.blade.php
+++ FRONTEND-USER/resources/views/Vuser/tamplate.blade.php
@@ -63,51 +63,6 @@
                             <li class="nav-item {{ Request::is('Vuser/lokasi') ? 'active' : '' }}">
                                 <a class="nav-link" href="{{ url('Vuser/lokasi') }}">Lokasi Toko</a>
                             </li>
-                            <!-- <li class="nav-item submenu dropdown">
-                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Shop</a>
-                                <ul class="dropdown-menu">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="category.html">Shop Category</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="single-product.html">Product Details</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="checkout.html">Product Checkout</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="cart.html">Shopping Cart</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="confirmation.html">Confirmation</a>
-                                    </li>
-                                </ul>
-                            </li> -->
-                            <!-- <li class="nav-item submenu dropdown">
-                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Blog</a>
-                                <ul class="dropdown-menu">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="blog.html">Blog</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="single-blog.html">Blog Details</a>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li class="nav-item submenu dropdown">
-                                <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Pages</a>
-                                <ul class="dropdown-menu">
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="login.html">Login</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="tracking.html">Tracking</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link" href="elements.html">Elements</a>
-                                    </li>
-                                </ul>
-                            </li> -->
                             <li class="nav-item {{ Request::is('Vuser/contact') ? 'active' : '' }}">
                                 <a class="nav-link" href="{{ url('Vuser/contact') }}">Contact</a>
                             </li>
