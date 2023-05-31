e3e8fce83b99ef164914018c2ae3c25b2000ebcf
setiawanrifaldo19 <setiawan_rifaldo@teknokrat.ac.id>
2023-05-31 22:03:21 +0700

    setiawan - buat view dashboard

---
+++ FRONTEND-ADMIN/resources/views/template/script.blade.php
@@ -0,0 +1,6 @@
+<!-- jQuery -->
+<script src="{{asset('templateAdmin/plugins/jquery/jquery.min.js')}}"></script>
+<!-- Bootstrap 4 -->
+<script src="{{asset('templateAdmin/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+<!-- AdminLTE App -->
+<script src="{{asset('templateAdmin/dist/js/adminlte.min.js')}}"></script>
